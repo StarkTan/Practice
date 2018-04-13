02b0a7885618e99800fbf0f5d77892fc357e22c4
Stark <386707933@qq.com>
2018-04-13 13:47:39 +0800

    使用Netty发送http请求

---
+++ NettyPractice/NettyHttpClient/src/main/java/com/stark/client/HttpClient.java
@@ -0,0 +1,59 @@
+package com.stark.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.*;
+
+import java.net.URI;
+
+/**
+ * Created by Stark on 2018/4/12.
+ * 下发消息的httpclient
+ */
+public class HttpClient {
+
+    public void connect(String host, int port) throws Exception {
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.channel(NioSocketChannel.class)
+                    .group(workGroup)
+                    .option(ChannelOption.SO_KEEPALIVE, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new HttpResponseDecoder());
+                            pipeline.addLast(new HttpRequestEncoder());
+                            pipeline.addLast(new HttpClientInboundHandler());
+                        }
+                    });
+            // Start the client.
+            ChannelFuture f = bootstrap.connect(host, port).sync();
+
+            DefaultFullHttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET,
+                    "/test", Unpooled.wrappedBuffer("test".getBytes("UTF-8")));
+
+            // 构建http请求
+            request.headers().set(HttpHeaderNames.HOST, host);
+            request.headers().set(HttpHeaderNames.CONNECTION, HttpHeaderValues.KEEP_ALIVE);
+            request.headers().set(HttpHeaderNames.CONTENT_LENGTH, request.content().readableBytes());
+            // 发送http请求
+            f.channel().write(request);
+            f.channel().flush();
+            f.channel().closeFuture().sync();
+        } finally {
+            workGroup.shutdownGracefully();
+        }
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        new HttpClient().connect("127.0.0.1", 8080);
+    }
+}
--- NettyPractice/README.md
+++ NettyPractice/README.md
@@ -4,3 +4,4 @@ WebContainer：使用Netty搭建web容器
     启动入口NettyWeb类 服务器类WebServer   数据处理配置类WebServerInitializer    请求处理类RequestHandler
     2.支持https 访问
     3.引入servlet 使用servlet 简单处理逻辑
+NettyHttpClient:使用Netty发送http请求
