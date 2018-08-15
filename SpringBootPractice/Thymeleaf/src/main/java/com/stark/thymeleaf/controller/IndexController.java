8daeb7b899bf3c9c0f6c4a99298859c859b7f8e5
Stark <386707933@qq.com>
2018-08-15 15:42:41 +0800

    SpringBoot + Thymeleaf

---
+++ SpringBootPractice/Thymeleaf/src/main/java/com/stark/thymeleaf/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.stark.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"/", "/index"})
+    public String index(Model model) {
+        model.addAttribute("msg", "welcome you!");
+        return "index";
+    }
+}
