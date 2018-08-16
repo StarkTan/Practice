665c2107ee56688f949dd63ead97d22febc2de71
Stark <386707933@qq.com>
2018-08-16 15:28:51 +0800

    SpringBoot + RESTful

---
+++ SpringBootPractice/RESTful/src/main/java/com/stark/restful/entity/BaseResponse.java
@@ -0,0 +1,16 @@
+package com.stark.restful.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BaseResponse<T> {
+    private boolean success;
+    private String msg;
+    private T data;
+}
---
+++ SpringBootPractice/RESTful/src/main/java/com/stark/restful/entity/User.java
@@ -0,0 +1,12 @@
+package com.stark.restful.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    private Long id;
+    private String name;
+    private Integer age;
+}
