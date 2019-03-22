95eeaca45ad801446e879283bed8caa9085180cd
Stark <386707933@qq.com>
2019-03-22 17:15:12 +0800

    使用 Stomp 协议

--- SpringBootPractice/WebSocket/src/main/java/com/stark/websocket/config/WebSocketConfig.java
+++ SpringBootPractice/WebSocket/src/main/java/com/stark/websocket/config/WebSocketConfig.java
@@ -6,10 +6,9 @@ import com.stark.websocket.handler.TestHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 import javax.validation.constraints.NotNull;
@@ -17,8 +16,18 @@ import javax.validation.constraints.NotNull;
 
 @Configuration
 @EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketConfigurer, WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/sockjs/stomp").withSockJS();
+    }
 
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
---
+++ SpringBootPractice/WebSocket/src/main/java/com/stark/websocket/controller/StompController.java
@@ -0,0 +1,49 @@
+package com.stark.websocket.controller;
+
+import com.stark.websocket.entity.Greeting;
+import com.stark.websocket.entity.HelloMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.HtmlUtils;
+
+@Controller
+public class StompController {
+
+    @Autowired
+    private SimpMessagingTemplate template;
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+    @RequestMapping("/sendall")
+    public void sendAll() {
+        template.convertAndSend("/topic/greetings", new Greeting("Send All"));
+    }
+
+    @SubscribeMapping("/greetings")
+    public Greeting sub() {
+        return new Greeting("Hello ");
+    }
+
+    /*@MessageMapping("/sendTest")
+    @SendTo("/topic/subscribeTest")
+    public String sendDemo(String message) {
+        System.out.println(message);
+        template.convertAndSend("/topic/subscribeTest", "服务器主动推的数据");
+        return "你发送的服务返回消息为:" + message;
+    }
+
+    @SubscribeMapping("/subscribeTest")
+    public String sub() {
+        return "感谢你订阅了我。。。";
+    }*/
+}
